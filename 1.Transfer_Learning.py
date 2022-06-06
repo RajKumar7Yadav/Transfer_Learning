99326c39e7fa923a857b7259c407395e27f2adab
RajKumar7Yadav <102537550+RajKumar7Yadav@users.noreply.github.com>
2022-06-06 21:38:20 +0530

    Add files via upload

--- 1.Transfer_Learning.py
+++ 1.Transfer_Learning.py
@@ -8,26 +8,26 @@ from keras.models import Model
 from keras.layers import Dense
 from keras.models import load_model
 
-# model=load_model("FaceQnet.h5")
-# #model.summary()
+model=load_model("FaceQnet.h5")
+#model.summary()
 
-# # check no of layers in old model
-# count=0
-# for i,layer in enumerate(model.layers):
-#     count=count+1
-# print(i)
+# check no of layers in old model
+count=0
+for i,layer in enumerate(model.layers):
+    count=count+1
+print(i)
 
-# # this method just count and create model till 2nd last, basically one layer is from below is removed
-# layer_name='layer_extra1'
-# model1=Model(inputs=model.input,outputs=model.get_layer(layer_name).output)
-# #model1.summary()
-# #model1.save("try.h5")
+# this method just count and create model till 2nd last, basically one layer is from below is removed
+layer_name='layer_extra1'
+model1=Model(inputs=model.input,outputs=model.get_layer(layer_name).output)
+#model1.summary()
+#model1.save("try.h5")
 
-# prediction=Dense(5, activation='softmax')(model1.layers[-1].output)
-# model_new=Model(inputs=model1.input,outputs=prediction)
-# model_new.compile(loss='categorical_crossentropy',optimizer='adam')
-# model_new.summary()
-# #model1.save("FaceQnet_new.h5")
+prediction=Dense(5, activation='softmax')(model1.layers[-1].output)
+model_new=Model(inputs=model1.input,outputs=prediction)
+model_new.compile(loss='categorical_crossentropy',optimizer='adam')
+model_new.summary()
+#model1.save("FaceQnet_new.h5")
 
 # Now try this with the Vgg16 model
 from keras.applications.vgg16 import VGG16
